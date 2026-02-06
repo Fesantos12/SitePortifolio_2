d5e6aa686eb92d36c160a88cf3f3c599ffe99b8c
Fellipe Santos <fellipeuker12@gmail.com>
2026-02-05 23:05:44 -0300

    header mobile separado em um componente a parte

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,5 @@
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet"
-import { Button } from '@headlessui/react'
-import { Menu as MenuHamburguer } from 'lucide-react';
+import { HeaderMobile } from "./HeaderMobile";
+
 
 export function Header() {
 
@@ -23,28 +13,7 @@ export function Header() {
 
         {/* dropdawn menu */}
 
-        <div className="md:hidden">
-          <Sheet>
-            <SheetTrigger className="cursor-pointer"><MenuHamburguer/></SheetTrigger>
-            <SheetContent className="bg-gray-200">
-              <SheetHeader>
-                <SheetTitle>Menu</SheetTitle>
-                <div className="w-full flex flex-col items-center gap-16 mt-20">
-                  <div className="flex flex-col gap-4 items-center">
-                    <a href="#">About</a>
-                    <a href="#">Experience</a>
-                    <a href="#">Projects</a>
-                    <a href="#">Contact</a>
-                  </div>
-                  <div className="flex flex-col gap-4">
-                    <Button className="border-2 border-gray-300 rounded-sm px-4 py-1 cursor-pointer hover:bg-gray-300">Resume</Button>
-                    <Button className="border-2 border-gray-300 rounded-sm px-4 py-1 cursor-pointer hover:bg-gray-300">Hire Me</Button>
-                  </div>
-                </div>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
-        </div>
+        <HeaderMobile/>
         
         
       </div>
