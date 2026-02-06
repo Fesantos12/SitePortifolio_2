f2d2f92cab9a13a0e118a77939b7844d18e21be7
Fellipe Santos <fellipeuker12@gmail.com>
2026-02-05 23:01:19 -0300

    header mobile pronto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu"
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 import { Button } from '@headlessui/react'
 import { Menu as MenuHamburguer } from 'lucide-react';
 
@@ -16,30 +18,34 @@ export function Header() {
 
         {/* mobile header */}
 
-      <div className="w-full h-22.5 border px-6.5 py-9 flex items-center justify-between">
+      <div className="w-full h-22.5 px-6.5 py-9 flex items-center justify-between">
         <div>Fellipe Santos</div>
 
         {/* dropdawn menu */}
 
-        <NavigationMenu className=" md:hidden">
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                <NavigationMenuTrigger><MenuHamburguer/></NavigationMenuTrigger>
-                <NavigationMenuContent className="bg-gray-300 rounded-sm p-4 border-none flex flex-col items-center">
-                    <NavigationMenuLink>About</NavigationMenuLink>
-                    <NavigationMenuLink>Experience</NavigationMenuLink>
-                    <NavigationMenuLink>Projects</NavigationMenuLink>
-                    <NavigationMenuLink>Contact</NavigationMenuLink>
-                    <NavigationMenuLink><Button className="rounded bg-gray-300 border border-gray-600 px-5 py-2 text-sm text-black data-active:bg-gray-400 data-hover:bg-gray-400">
-                        Resume
-                    </Button></NavigationMenuLink>
-                    <NavigationMenuLink><Button className="rounded bg-gray-300 border border-gray-600 px-5 py-2 text-sm text-center text-black data-active:bg-gray-400 data-hover:bg-gray-400">
-                        Hire Me
-                    </Button></NavigationMenuLink>
-                </NavigationMenuContent>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger className="cursor-pointer"><MenuHamburguer/></SheetTrigger>
+            <SheetContent className="bg-gray-200">
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+                <div className="w-full flex flex-col items-center gap-16 mt-20">
+                  <div className="flex flex-col gap-4 items-center">
+                    <a href="#">About</a>
+                    <a href="#">Experience</a>
+                    <a href="#">Projects</a>
+                    <a href="#">Contact</a>
+                  </div>
+                  <div className="flex flex-col gap-4">
+                    <Button className="border-2 border-gray-300 rounded-sm px-4 py-1 cursor-pointer hover:bg-gray-300">Resume</Button>
+                    <Button className="border-2 border-gray-300 rounded-sm px-4 py-1 cursor-pointer hover:bg-gray-300">Hire Me</Button>
+                  </div>
+                </div>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+        </div>
+        
         
       </div>
     </>
