96825b0ed26026c5d7f30e70bb983c16fdaae4cb
Fellipe Santos <fellipeuker12@gmail.com>
2026-02-10 18:34:44 -0300

    responsividade sessão about feita

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,27 +1,46 @@
 import Img from "@/assets/eu ia.jpeg";
-import { BlurFade } from "@/components/ui/blur-fade"
+import { BlurFade } from "@/components/ui/blur-fade";
 import { ReusableTitle } from "./Reusableitle";
 
 export const About = () => {
-    return (
-        <>
-            <div id="about" className="w-full h-screen px-6.5 py-9 bg-gray-300 flex flex-col items-center justify-around">
-                <ReusableTitle text="About Me"/>
-                <BlurFade delay={0.25} inView>
-                    My name is Fellipe, I'm 24 years old, I've been studying programming for almost 6 years and I'm 
-                    constantly seeking improvement in the field. <br/><br/>
-
-                    I started studying programming in 2020 with a 100-hour HTML and CSS course, followed by a JavaScript 
-                    course on Udemy. Then I took a ReactJS course, also on Udemy, but since I wanted to broaden my 
-                    knowledge, I took courses on React Native, NextJS, and NodeJS on YouTube. <br/><br/>
-
-                    Currently, I'm looking for a job as a Junior Developer and I'm always studying and practicing to 
-                    reach my goal. <br/><br/>
-                </BlurFade>
-                <BlurFade delay={0.25} inView className="w-37.5 h-37.5 rounded-md bg-gray-400">
-                    <img src={Img} alt="Foto de Fellipe" width={150} height={150} className="rounded-md w-37.5 h-37.5"/>
-                </BlurFade>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div
+        id="about"
+        className="w-full h-screen px-6.5 py-9 bg-gray-300 flex flex-col items-center justify-around md:py-30 md:px-63.75 md:gap-12.5"
+      >
+        <ReusableTitle text="About Me" />
+        <div className="w-full flex flex-col items-center gap-7.5 md:flex-row">
+          <BlurFade delay={0.25} inView className="md:flex-1">
+            My name is Fellipe, I'm 24 years old, I've been studying programming
+            for almost 6 years and I'm constantly seeking improvement in the
+            field. <br />
+            <br />
+            I started studying programming in 2020 with a 100-hour HTML and CSS
+            course, followed by a JavaScript course on Udemy. Then I took a
+            ReactJS course, also on Udemy, but since I wanted to broaden my
+            knowledge, I took courses on React Native, NextJS, and NodeJS on
+            YouTube. <br />
+            <br />
+            Currently, I'm looking for a job as a Junior Developer and I'm
+            always studying and practicing to reach my goal. <br />
+            <br />
+          </BlurFade>
+          <BlurFade
+            delay={0.25}
+            inView
+            className="w-37.5 h-37.5 rounded-md bg-gray-400 md:w-75 md:h-75"
+          >
+            <img
+              src={Img}
+              alt="Foto de Fellipe"
+              width={150}
+              height={150}
+              className="rounded-md w-37.5 h-37.5 md:w-75 md:h-75 md:object-cover"
+            />
+          </BlurFade>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/Reusableitle.tsx
+++ src/components/Reusableitle.tsx
@@ -1,15 +1,21 @@
-import { BlurFade } from "./ui/blur-fade"
+import { BlurFade } from "./ui/blur-fade";
 
 type ReusableTitleProps = {
-    text: string;
-}
+  text: string;
+};
 
 export const ReusableTitle = ({ text }: ReusableTitleProps) => {
-    return (
-        <BlurFade delay={0.25} inView className="w-full flex items-center justify-center gap-2">
-                <div className="w-full h-0.5 bg-black"></div>
-                <div className="font-normal text-3xl w-full text-center">{text}</div>
-                <div className="w-full h-0.5 bg-black"></div>
-        </BlurFade>
-    )
-}
+  return (
+    <BlurFade
+      delay={0.25}
+      inView
+      className="w-full flex items-center justify-center gap-2"
+    >
+      <div className="w-full h-0.5 bg-black"></div>
+      <div className="font-normal text-3xl w-full text-center md:text-4xl">
+        {text}
+      </div>
+      <div className="w-full h-0.5 bg-black"></div>
+    </BlurFade>
+  );
+};
