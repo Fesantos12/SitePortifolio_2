ed59656572fc0f35019dc660ed6fb7e74799fc16
Fellipe Santos <fellipeuker12@gmail.com>
2026-02-10 18:19:14 -0300

    responsividade sessão hero feita

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,15 +1,20 @@
-import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern"
-import { TypingAnimation } from "@/components/ui/typing-animation"
-import { ChevronsDown } from 'lucide-react';
+import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern";
+import { TypingAnimation } from "@/components/ui/typing-animation";
+import { ChevronsDown } from "lucide-react";
 
 export const HeroSection = () => {
   return (
     <section className="w-full h-[calc(100vh-90px)] px-6.5 py-9 flex flex-col items-center justify-center gap-20">
-        <AnimatedGridPattern className="h-[calc(100vh-90px)] mt-22.5"/>
-        <div className="w-full flex flex-col items-center gap-2">
-            <TypingAnimation className="text-4xl font-bold" loop>Hi, I’m Fellipe Santos</TypingAnimation>
-            <div className="text-[20px] font-light">I'm Full-Stack Developer</div>
+      <AnimatedGridPattern className="h-[calc(100vh-90px)] mt-22.5" />
+      <div className="w-full flex flex-col items-center gap-2">
+        <TypingAnimation className="text-4xl font-bold md:text-[72px]" loop>
+          Hi, I’m Fellipe Santos
+        </TypingAnimation>
+        <div className="text-[20px] font-light md:text-2xl">
+          I'm Full-Stack Developer
         </div>
-        <ChevronsDown height={32} width={32}/>
+      </div>
+      <ChevronsDown height={32} width={32} />
     </section>
-    )}
+  );
+};
