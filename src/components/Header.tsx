490b3e8efb982b6eb4b60b4a422410cc1d2d1121
Fellipe Santos <fellipeuker12@gmail.com>
2026-02-10 18:15:15 -0300

    responsividade do header feita

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,30 @@
 import { HeaderMobile } from "./HeaderMobile";
 
-
 export const Header = () => {
-
   return (
     <>
-
-        {/* mobile header */}
-
-      <div className="w-full h-22.5 px-6.5 py-9 flex items-center justify-between">
+      <div className="w-full h-22.5 px-6.5 py-9 flex items-center justify-between md:flex">
         <div>Fellipe Santos</div>
 
-        {/* dropdawn menu */}
-        <HeaderMobile/>
+        {/* mobile header */}
+        <HeaderMobile />
 
+        <div className="flex items-center gap-5 max-md:hidden">
+          <a href="#">About</a>
+          <a href="#">Experience</a>
+          <a href="#">Projects</a>
+          <a href="#">Contact</a>
+        </div>
 
-        
-        
+        <div className="flex items-center gap-5 max-md:hidden">
+          <button className="w-20.25 h-8.75 border border-gray-300 rounded-sm hover:bg-gray-300 cursor-pointer">
+            Resume
+          </button>
+          <button className="w-20.25 h-8.75 border border-gray-300 rounded-sm hover:bg-gray-300 cursor-pointer">
+            Hire Me
+          </button>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
